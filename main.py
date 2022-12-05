90f2e2d9b058022871339fea04b32631e8a5538f
Ahanna165 <hanna.ahafonova@online.de>
2022-12-05 11:04:28 +0100

    file control

--- main.py
+++ main.py
@@ -3,24 +3,40 @@ from hostedcatalog import *
 from hostedcatalog import directory, inputdata, outputdata
 from hostedcatalog.directory import Directory
 from hostedcatalog.outputdata import *
+import openpyxl
 
 #new_folder = directory.Directory()
 #directory.Directory.set_directory(new_folder)
 
-inputfile = inputdata.InputFile()
-inputfile = inputfile.impute_na()
-inputfile = inputfile[inputfile['VOLUMEPRICE'] != 0]
-inputfile = inputfile[inputfile['QTYAMOUNT'] == 1]
-inputfile = inputfile.drop_duplicates(subset=['PARTNUM'])
-inputfile = inputdata.InputFile.set_unit(inputfile)
-inputfile = inputdata.InputFile.sup(inputfile)
-inputfile = inputdata.InputFile.set_columns(inputfile)
-inputdata.InputFile.get_summary(inputfile)
+data = str(input('Paste here the PATH of your input file as EXCEL: ')).replace('"', '')
+while True:
+    if data.endswith('xlsx'):
+        df = pd.read_excel(data, engine='openpyxl')
+        print('File format is .xlsx')
+        break
+    elif data.endswith('xls'):
+        df = pd.read_excel(data, engine='xlrd')
+        print('File format is .xls')
+        break
+    else:
+        print('File format is invalid')
 
-newcat = OutputFile.from_file()
-newcat = newcat.cat_join(inputfile)
+print(df.head())
 
-newcat = outputdata.OutputFile.new_column(newcat) #not working
-newcat['MIME_LRG'] = outputdata.OutputFile.mime_lrg(newcat) # not working
+#inputfile= InputFile(df)
+#inputfile = InputFile(inputfile)
+#inputfile = inputfile[inputfile['VOLUMEPRICE'] != 0]
+#inputfile = inputfile[inputfile['QTYAMOUNT'] == 1]
+#inputfile = inputfile.drop_duplicates(subset=['PARTNUM'])
+#inputfile = inputdata.InputFile.set_unit(inputfile)
+#inputfile = inputdata.InputFile.sup(inputfile)
+#inputfile = inputdata.InputFile.set_columns(inputfile)
+#inputdata.InputFile.get_summary(inputfile)
 
-new_folder(inputfile)
+#newcat = OutputFile.from_file()
+#newcat = newcat.cat_join(inputfile)
+
+#newcat = outputdata.OutputFile.new_column(newcat) #not working
+#newcat['MIME_LRG'] = outputdata.OutputFile.mime_lrg(newcat) # not working
+
+#new_folder(inputfile)
